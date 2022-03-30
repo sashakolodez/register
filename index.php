dc5c099cc52197bf6e37d377985f68fc8f475fc6
sashakolodez <sashakolodez@bk.ru>
2022-03-30 13:48:33 +0400

    изменения

--- index.php
+++ index.php
@@ -8,44 +8,35 @@
     <title>Document</title>
 </head>
 <body>
+<div>
     <center><div class="form">
     <h1 class="reg">Регистрация</h1>    
     <form action="index.php" method="post">
-        <input type="text" placeholder="введите имя" name="username" id="name">
-        <br> <input type="password" placeholder="введите пароль" name="userpass" id="pass">
-       <br> <input type="email" placeholder="введите почту" name="useremail" id="email">
-       <br> <input type="submit" value="зарегистрироватся" id="button">
-    </form></div></center>
+       <p> <input type="text" placeholder="введите имя" name="username" id="name"></p>
+       <p>  <input type="password" placeholder="введите пароль" name="userpass" id="pass"></p>
+       <p> <input type="email" placeholder="введите почту" name="useremail" id="email"></p>
+       <p> <input type="submit" value="зарегистрироватся" id="button"></p>
+    </form>
+</div>
+</center>
+</div>
     <?php 
     $name = $_POST['username'];
     $pass = $_POST['userpass'];
     $email = $_POST['useremail'];
-    if($name == 1)
-    echo "вы ввели не коректное имя";
-    else if(strlen($name) == 2)
-    echo "вы ввели не коректное имя";
-   else if(strlen($name) == 3)
-   echo "вы ввели не коректное имя";
-    else if(strlen($pass) == 1)
-    echo "не надёжный пароль";
-    else if(strlen($pass) == 2)
-    echo "не надёжный пароль";
-    else if(strlen($pass) == 3)
-    echo "не надёжный пароль";
-    else if(strlen($pass) == 4)
-    echo "не надёжный пароль";
-    else if(strlen($pass) == 5)
-    echo "не надёжный пароль";
+    if(strlen($name) <=3 && strlen($name) !==0)
+    echo "введите коректное имя";
+    else if(strlen($pass) <=5 && strlen($pass) !==0)
+    echo "введите надежный пароль" ;
     else if($name !== "" || $pass !== ""){
         $userpass = md5($pass);
         $mysql = new mysqli ("localhost","mysql","", "reg");
         $mysql->query("SET NAMES 'utf8'");
-$mysql->query("INSERT INTO `register`(`name`,`pass`, `email`) VALUES('$name', '$userpass','$email')");
+$mysql->query("INSERT INTO `register`(`name`,`pass`, `email`) VALUES({$name}, {$userpass},{$email})");
         $mysql->close();
-//echo "<script>window.location.href='glav.php'</script>";
-//exit;
     }
     ?>
 
 </body>
 </html>
+
